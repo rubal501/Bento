47f6c79e96fa05cfbbea1a67eedf366c1ab62efd
Roberto <rubal820@gmail.com>
2021-07-09 17:02:03 -0500

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -3,15 +3,15 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = ' John Doe';
+const name = ' Roberto';
 
 // Here you can change your greetings
-const gree1 = 'Go to Sleep!  ';
-const gree2 = 'Good morning!  ';
-const gree3 = 'Good afternoon  ';
-const gree4 = 'Good evening,  ';
-const gree5 = 'Good evening,  ';
-const gree6 = 'Good evening,  ';
+const gree1 = 'Vamos a mimir!  ';
+const gree2 = 'Buenos dias!  ';
+const gree3 = 'Buenas!';
+const gree4 = 'Buenas tardes,  ';
+const gree5 = 'Buenas tardes,  ';
+const gree6 = 'Buenas noches,  ';
 
 // Define the hours of the greetings
 if (hour >= 23 && hour < 5) {
